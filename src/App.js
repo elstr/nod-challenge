091632ac010985f3391304cf7f2ebcac0b841ba8
elstr <eleonora.lester@gmail.com>
2020-03-01 08:39:25 -0300

    feat: Render minimal UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+import dataBase from "./database.json";
+
+import { Text, DropDown } from "./components";
+
+const App = () => {
+  const [fields, setFields] = useState([]);
+  const [error, setError] = useState(false);
+  const [reset, setFormReset] = useState(false);
+
+  const saveValue = field => {
+   console.log("save value")
+   console.log(field)
+  };
+
+  const saveToCSV = () => {
+    console.log("save csv")
+    setFormReset(true)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {dataBase.map((field, index) => {
+        switch (field.type) {
+          case "dropdown":
+          case "multi-select":
+            return (
+              <DropDown
+                handleChange={value => saveValue(value)}
+                key={`field-${index}`}
+                field={field}
+                reset={reset}
+              />
+            );
+          default:
+            return (
+              <Text
+                handleBlur={value => saveValue(value)}
+                key={`field-${index}`}
+                field={field}
+                reset={reset}
+              />
+            );
+        }
+      })}
+
+      <button onClick={() => saveToCSV()} disabled={error}>
+        Submit
+      </button>
     </div>
   );
-}
+};
 
 export default App;
