f0e1c979bd3822d423def28fb85d31385122dc22
elstr <eleonora.lester@gmail.com>
2020-03-01 09:45:20 -0300

    feat: Check if has errors logic

--- src/App.js
+++ src/App.js
@@ -5,14 +5,34 @@ import dataBase from "./database.json";
 
 import { Text, DropDown } from "./components";
 
+// TODO: Move this to useEffect onMount
+// iterate dataBase and add error to each object
+const INITIAL_STATE = {
+  "First Name": { value: "", error: true },
+  "Last Name": { value: "", error: true },
+  "Date of Birth": { value: "", error: true },
+  Email: { value: "", error: true },
+  Address: { value: "", error: true },
+  "Contact Number": { value: "", error: true },
+  Gender: { value: "", error: true },
+  "Areas of Recommendation": { selectedValues: "", error: true }
+};
+
 const App = () => {
-  const [fields, setFields] = useState([]);
-  const [error, setError] = useState(false);
+  const [fields, setFields] = useState(INITIAL_STATE);
+  const [error, setError] = useState(true);
   const [reset, setFormReset] = useState(false);
 
+  useEffect(() => {
+    const hasError = Object.values(fields).map(f => f.error).includes(true);
+    hasError ? setError(true) : setError(false);
+  }, [fields]);
+
   const saveValue = field => {
-   console.log("save value")
-   console.log(field)
+    // Update fields with new field values
+    const newFields = Object.assign({}, fields);
+    newFields[field.name] = Object.assign({}, field);
+    setFields(newFields);
   };
 
   const saveToCSV = () => {
